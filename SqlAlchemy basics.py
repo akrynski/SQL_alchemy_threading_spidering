34a18f8c3afd067127e22fdebe3159139d3f0f19
akrynski <akrynski@users.noreply.github.com>
2024-02-04 08:08:55 +0100

    Dodany nagłówek w SqlAlchemy basics.py i kilka zdań więcej o ORM

--- SqlAlchemy basics.py
+++ SqlAlchemy basics.py
@@ -14,19 +14,38 @@ from sqlalchemy.orm import Mapped
 from sqlalchemy.orm import mapped_column
 from sqlalchemy.orm import relationship
 
-'''
+"""
 Modele deklaratywne ORM. 
 Tutaj definiujemy konstrukcje na poziomie modułu, które utworzą struktury, 
 do których będziemy wysyłać zapytania z bazy danych. 
 Struktura ta, znana jako mapowanie deklaratywne, definiuje jednocześnie zarówno model obiektowy Pythona, 
 jak i metadane bazy danych opisujące rzeczywiste tabele SQL, które istnieją lub będą istnieć w określonej bazie danych
-'''
+"""
 
-
+"""
+Mapowanie zaczynamy tworząc od DeclarativeBase pustą klasę Base
+"""
 class Base(DeclarativeBase):
     pass
 
+"""
+Poszczególne odwzorowane klasy są następnie tworzone przez tworzenie podklas . 
+Mapowana klasa zazwyczaj odwołuje się do jednej konkretnej tabeli bazy danych, 
+której nazwa jest wskazywana za pomocą atrybutu klasy Base__tablename__
 
+Następnie przez wpisanie specjalnej adnotacji Mapped deklarowane są kolumny tabeli, i ich atrybuty. 
+Nazwa każdego atrybutu odpowiada kolumnie, która ma być częścią tabeli bazy danych. 
+Typ danych każdej kolumny jest pobierany najpierw z typu danych Pythona. 
+Dopuszczanie wartości null wynika z tego, czy lub nie jest używany modyfikator typu. 
+Bardziej szczegółowe informacje na temat wpisywania mogą być wskazane za pomocą obiektów typu SQLAlchemy 
+po prawej stronie dyrektywy mapped_column, takich jak mapped_column(String(30) datatype użyte do stworzenia i określenia
+typu kolumny User.name poniżej czy też określenia i powiązania kluczy tabel - wszystkie klasy mapowane ORM wymagają, 
+aby co najmniej jedna kolumna była zadeklarowana jako primary_key. 
+Skojarzenie między typami języka Python a typami SQL można dostosować za pomocą mapy adnotacji typów opisanej tutaj:
+https://docs.sqlalchemy.org/en/20/orm/declarative_tables.html#orm-declarative-mapped-column-type-map
+Podsumowując, 
+KOMBINACJA NAZWY TABELI ORAZ LISTY DEKLARACJI KOLUMN JEST ZNANA W SQLALCHEMY JAKO METADANE TABELI. 
+"""
 class User(Base):
     __tablename__ = "user_account"
     id: Mapped[int] = mapped_column(primary_key=True)
@@ -38,7 +57,13 @@ class User(Base):
 
     def __repr__(self) -> str:
         return f"User(id={self.id!r}, name={self.name!r}, fullname={self.fullname!r})"
-
+"""
+W przeciwieństwie do mapped_column czyli atrybutów opartych na kolumnach  
+relationship() oznacza połączenie między dwiema klasami ORM. 
+ Tutaj, w klasach User i Address, User.addresses linkuje User do Address, 
+ natomiast Address.user linkuje Address do User.
+ Więcej o relacjach: https://docs.sqlalchemy.org/en/20/tutorial/orm_related_objects.html#tutorial-orm-related-objects
+"""
 
 class Address(Base):
     __tablename__ = "address"
@@ -51,9 +76,14 @@ class Address(Base):
         return f"Address(id={self.id!r}, email_address={self.email_address!r})"
 
 
-'''
+"""
+ENGINE jest to fabryka, która może tworzyć nowe połączenia z bazą danych, 
+które również przechowują połączenia wewnątrz puli połączeń w celu szybkiego ponownego użycia. 
+Do nauki zwykle używamy bazy danych SQLite tworzonej w pamięci ROM: "sqlite://"
 
-'''
+Parametr echo=True wskazuje, że kod SQL emitowany przez połączenia będzie wysłany do standardowego wyjścia STDOUT
+(zwykle wypisany w konsoli).
+"""
 
 from sqlalchemy import create_engine
 
@@ -80,3 +110,25 @@ with Session(engine) as session:
     patrick = User(name="patrick", fullname="Patrick Star")
     session.add_all([spongebob, sandy, patrick])
     session.commit()
+"""
+---------------------       SELECT
+Metodą, która w powiązaniu z select() jest często przydatna podczas wykonywania zapytań o obiekty ORM 
+jest  Session.scalars()
+
+"""
+from sqlalchemy import select
+
+session = Session(engine)
+
+stmt = select(User).where(User.name.in_(["spongebob", "sandy"]))
+
+for user in session.scalars(stmt):
+    print(f"{20*'>'}Users: {user}\n")
+stmt = (
+    select(Address)
+    .join(Address.user)
+    .where(User.name == "sandy")
+    .where(Address.email_address == "sandy@sqlalchemy.org")
+)
+sandy_address = session.scalars(stmt).one()
+print(f"{20*'>'}Sandy's address: {sandy_address}")
